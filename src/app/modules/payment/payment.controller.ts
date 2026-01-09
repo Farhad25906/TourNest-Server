2ad3d9142732a63408aee51f50ff679fa67409c2
Farhad25906 <farhadhossen9036@gmail.com>
2026-01-10 02:05:31 +0600

    WebHook Updated

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,4 +1,3 @@
-
 import { Request, Response } from "express";
 import catchAsync from "../../shared/catchAsync";
 import { PaymentService } from "./payment.service";
@@ -6,28 +5,30 @@ import sendResponse from "../../shared/sendResponse";
 import { stripe } from "../../config/stripe";
 import httpStatus from "http-status";
 
-const handleStripeWebhookEvent = catchAsync(async (req: Request, res: Response) => {
-  const sig = req.headers["stripe-signature"] as string;
-  // const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
-  const webhookSecret = "whsec_ac47ba4152409aae661deca87c5fc76fd4345247757c8b4cb5250f8f8d9d7ba1"
+const handleStripeWebhookEvent = catchAsync(
+  async (req: Request, res: Response) => {
+    const sig = req.headers["stripe-signature"] as string;
+    // const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+    // const webhookSecret = "whsec_ac47ba4152409aae661deca87c5fc76fd4345247757c8b4cb5250f8f8d9d7ba1"
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!;
+    let event;
+    try {
+      event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
+    } catch (err: any) {
+      console.error("⚠️ Webhook signature verification failed:", err.message);
+      return res.status(400).send(`Webhook Error: ${err.message}`);
+    }
 
-  let event;
-  try {
-    event = stripe.webhooks.constructEvent(req.body, sig, webhookSecret);
-  } catch (err: any) {
-    console.error("⚠️ Webhook signature verification failed:", err.message);
-    return res.status(400).send(`Webhook Error: ${err.message}`);
+    await PaymentService.handleStripeWebhookEvent(event);
+
+    sendResponse(res, {
+      statusCode: 200,
+      success: true,
+      message: "Webhook processed successfully",
+      data: null,
+    });
   }
-  
-  await PaymentService.handleStripeWebhookEvent(event);
-
-  sendResponse(res, {
-    statusCode: 200,
-    success: true,
-    message: 'Webhook processed successfully',
-    data: null,
-  });
-});
+);
 
 // Get user payment history (TOURIST)
 const getUserPayments = catchAsync(async (req: Request, res: Response) => {
