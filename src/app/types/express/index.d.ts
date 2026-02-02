c5e9166989602f08d8f554d02f644fd624e4184a
Farhad25906 <farhadhossen9036@gmail.com>
2026-02-02 08:02:41 +0600

    Trying to resolve render issue

--- src/app/types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { IJWTPayload } from '../../app/interfaces/common';
-
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IJWTPayload;
-    }
-  }
-}
--- src/app/types/index.d.ts
+++ src/app/types/index.d.ts
@@ -1,13 +1,14 @@
-// types/express/index.d.ts
 import { Host, Subscription } from '@prisma/client';
+import { IJWTPayload } from '../common';
 
 declare global {
   namespace Express {
     interface Request {
+      user?: IJWTPayload;
       host?: Host;
       subscription?: Subscription;
     }
   }
 }
 
-export {};
+export { };
